91f2b3896faa10f347dd9021959f90a2b1502d74
schrema8 <schrema8@students.zhaw.ch>
2021-03-15 10:43:18 +0100

    -made scenebuilder usable through generating the grapheditorcontainer through code, introduced second pane for menu

--- src/main/java/ch/zhaw/controller/MainController.java
+++ src/main/java/ch/zhaw/controller/MainController.java
@@ -8,15 +8,19 @@ import de.tesis.dynaware.grapheditor.core.view.GraphEditorContainer;
 import de.tesis.dynaware.grapheditor.model.GModel;
 import de.tesis.dynaware.grapheditor.model.GraphFactory;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class MainController {
     private final GraphEditor graphEditor = new DefaultGraphEditor();
-    @FXML
-    public GraphEditorContainer graphEditorContainer;
 
     private static final String STYLE_CLASS_TITLED_SKINS = "data-flow-diagram-skin";
+    @FXML
+    public VBox graphEditorParent;
 
     private DataFlowDiagramSkinController dfdSkinController;
 
@@ -28,11 +32,28 @@ public class MainController {
     public void initialize() {
         final GModel model = GraphFactory.eINSTANCE.createGModel();
         graphEditor.setModel(model);
+        GraphEditorContainer graphEditorContainer = new GraphEditorContainer();
+        setMaxSizeToInfinity(graphEditorContainer);
+        setMaxSizeToInfinity(graphEditorParent);
+        setGrowth(graphEditorContainer);
+        setGrowth(graphEditorParent);
+        graphEditorParent.getChildren().add(graphEditorContainer);
+
         graphEditorContainer.setGraphEditor(graphEditor);
         dfdSkinController = new DataFlowDiagramSkinController(graphEditor, graphEditorContainer);
         graphEditor.getView().getStyleClass().add(STYLE_CLASS_TITLED_SKINS);
         graphEditor.setConnectorValidator(new DataFlowConnectorValidator());
         graphEditor.getProperties().setGridVisible(true);
+    }
+
+    private void setGrowth(Node node) {
+        HBox.setHgrow(node, Priority.ALWAYS);
+        VBox.setVgrow(node, Priority.ALWAYS);
+    }
+
+    private void setMaxSizeToInfinity(Node node) {
+        node.maxHeight(Double.POSITIVE_INFINITY);
+        node.maxWidth(Double.POSITIVE_INFINITY);
     }
 
 
