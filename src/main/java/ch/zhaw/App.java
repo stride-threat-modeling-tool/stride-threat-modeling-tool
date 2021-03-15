302754e0d6d483e080743aff09fcc2ce947291b4
schrema8 <schrema8@students.zhaw.ch>
2021-03-15 10:41:33 +0100

    -set min size for window

--- src/main/java/ch/zhaw/App.java
+++ src/main/java/ch/zhaw/App.java
@@ -13,19 +13,23 @@ public class App extends Application {
     private static final String GENERAL_STYLESHEET = "styles.css";
     private static final String DATA_STORE_STYLESHEET = "DataStore.css";
     private static final String EXTERNAL_ENTITY_STYLESHEET = "ExternalEntity.css";
-    static MainController controller;
+    public static final int MIN_WIDTH = 800;
+    public static final int MIN_HEIGHT = 600;
+    private MainController controller;
     @Override
     public void start(final Stage stage) throws Exception {
         final URL location = getClass().getResource("main.fxml");
         final FXMLLoader loader = new FXMLLoader();
         final Parent root = loader.load(location.openStream());
-        Scene scene = new Scene(root, 830, 630);
+        Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
         addStyleSheetToScene(scene, DATA_STORE_STYLESHEET);
         addStyleSheetToScene(scene, EXTERNAL_ENTITY_STYLESHEET);
         addStyleSheetToScene(scene, GENERAL_STYLESHEET);
         stage.setScene(scene);
+        stage.setMinHeight(MIN_HEIGHT);
+        stage.setMinWidth(MIN_WIDTH);
         stage.show();
-       controller = loader.getController();
+        controller = loader.getController();
     }
 
     private void addStyleSheetToScene(Scene scene, String filename){
