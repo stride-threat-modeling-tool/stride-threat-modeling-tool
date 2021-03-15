ad40603d9a56f1b6d6bf3b8fab6491c20e06db24
schrema8 <schrema8@students.zhaw.ch>
2021-03-15 10:46:50 +0100

    -set initial window size to pref size

--- src/main/java/ch/zhaw/App.java
+++ src/main/java/ch/zhaw/App.java
@@ -15,13 +15,15 @@ public class App extends Application {
     private static final String EXTERNAL_ENTITY_STYLESHEET = "ExternalEntity.css";
     public static final int MIN_WIDTH = 800;
     public static final int MIN_HEIGHT = 600;
+    public static final int PREF_WIDTH = 1200;
+    public static final int PREF_HEIGHT = 1000;
     private MainController controller;
     @Override
     public void start(final Stage stage) throws Exception {
         final URL location = getClass().getResource("main.fxml");
         final FXMLLoader loader = new FXMLLoader();
         final Parent root = loader.load(location.openStream());
-        Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
+        Scene scene = new Scene(root, PREF_WIDTH, PREF_HEIGHT);
         addStyleSheetToScene(scene, DATA_STORE_STYLESHEET);
         addStyleSheetToScene(scene, EXTERNAL_ENTITY_STYLESHEET);
         addStyleSheetToScene(scene, GENERAL_STYLESHEET);
