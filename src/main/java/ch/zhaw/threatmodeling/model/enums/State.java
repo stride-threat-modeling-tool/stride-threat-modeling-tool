f4904293f899bcd5e68bce0a4bb25ee2fbba959a
schrema8 <schrema8@students.zhaw.ch>
2021-03-22 23:33:05 +0100

    -fixed typo

--- src/main/java/ch/zhaw/threatmodeling/model/enums/State.java
+++ src/main/java/ch/zhaw/threatmodeling/model/enums/State.java
@@ -3,7 +3,7 @@ package ch.zhaw.threatmodeling.model.enums;
 public enum State {
     NOT_STARTED("Not Started"),
     NEEDS_INVESTIGATION("Needs Investigation"),
-    NOT_APPLICABLE("Not applicable"),
+    NOT_APPLICABLE("Not Applicable"),
     MITIGATED("Mitigated");
 
 
