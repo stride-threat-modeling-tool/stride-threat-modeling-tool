0f050ed2f53247ce7c702a4f44b42881ed55c3df
schrema8 <schrema8@students.zhaw.ch>
2021-04-01 13:28:37 +0200

    -#17 closes, implemented positioning of connectors correctly

--- src/main/java/ch/zhaw/threatmodeling/skin/nodes/generic/ellipse/GenericEllipseNodeSkin.java
+++ src/main/java/ch/zhaw/threatmodeling/skin/nodes/generic/ellipse/GenericEllipseNodeSkin.java
@@ -13,6 +13,7 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.StrokeType;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -30,75 +31,24 @@ public abstract class GenericEllipseNodeSkin extends GenericNodeSkin implements 
 
     @Override
     public void layoutConnectors() {
-        layoutConnectors(topConnectorSkins, false, 0, false);
-        layoutConnectors(rightConnectorSkins, true, getRoot().getWidth(), false);
-        layoutConnectors(bottomConnectorSkins, false, getRoot().getHeight(), false);
-        layoutConnectors(leftConnectorSkins, true, 0, true);
-    }
-
-    private void layoutConnectors(final List<GConnectorSkin> connectorSkins, final boolean vertical, final double offset, final boolean left) {
-
-        final int count = connectorSkins.size();
-        final double yOffsetVertical = -DataFlowConnectorSkin.SIZE / 2.0;
-
-        for (int i = 0; i < count; i++) {
-
-            final GConnectorSkin skin = connectorSkins.get(i);
+        List<GConnectorSkin> allConnectors = new ArrayList<>(topConnectorSkins);
+        allConnectors.addAll(rightConnectorSkins);
+        allConnectors.addAll(bottomConnectorSkins);
+        allConnectors.addAll(leftConnectorSkins);
+        final double offset = -DataFlowConnectorSkin.SIZE / 2.0;
+        //formula from here:
+        //https://mathopenref.com/coordparamellipse.html
+        double currentRadian = 0;
+        final double incrementRadian = 2 * Math.PI / allConnectors.size();
+        for (GConnectorSkin skin: allConnectors) {
             final Node skinRoot = skin.getRoot();
-            if (vertical) {
-
-                final double offsetX = getMinorOffsetX(skin.getItem());
-
-                if (i % 2 != 0) {
-                    //center left/right connector
-                    skinRoot.setLayoutX(GeometryUtils.moveOnPixel(offset - skin.getWidth() / 2 + offsetX));
-                    skinRoot.setLayoutY((getRoot().getHeight() / (count - 1) * i) + yOffsetVertical);
-                } else {
-                    //corner connectors
-                    final Point2D pointToPlaceConnector = calcDiagonalOffset();
-
-                    LOGGER.info("y before " + skinRoot.getLayoutY());
-                    LOGGER.info("x before " + skinRoot.getLayoutX());
-                    skinRoot.setLayoutY(pointToPlaceConnector.getY() + skinRoot.getLayoutY());
-                    skinRoot.setLayoutX(pointToPlaceConnector.getX() + skinRoot.getLayoutX());
-                    LOGGER.info("y " + skinRoot.getLayoutY());
-                    LOGGER.info("x " + skinRoot.getLayoutX());
-
-                }
-
-            } else {
-
-                final double offsetX = getRoot().getWidth() / (count + 1);
-                final double offsetY = getMinorOffsetY(skin.getItem());
-
-                skinRoot.setLayoutX(GeometryUtils.moveOnPixel((i + 1) * offsetX - skin.getWidth() / 2));
-                skinRoot.setLayoutY(GeometryUtils.moveOnPixel(offset - skin.getHeight() / 2 + offsetY));
-            }
+            final double x = ellipseWithConnectors.getCenterX() + ellipseWithConnectors.getRadiusX() * Math.cos(currentRadian) + offset;
+            final double y = ellipseWithConnectors.getCenterY() + ellipseWithConnectors.getRadiusY() * Math.sin(currentRadian)+ offset;
+            skinRoot.setLayoutX(x);
+            skinRoot.setLayoutY(y);
+            currentRadian += incrementRadian;
         }
-    }
 
-    private Point2D calcDiagonalOffset() {
-        final double alpha = Math.toRadians(45);
-        final double c = calcRadiusAtInterSection(alpha);
-        LOGGER.info("c: " + c);
-        final double b = c / (1.0 / Math.cos(alpha)); // sec(alpha)
-        final double a = Math.sin(alpha) * c;
-        LOGGER.info("b: " + b);
-        LOGGER.info("a: " + a);
-        return new Point2D(a, b);
-
-    }
-
-    private double calcRadiusAtInterSection(double angleInRadians){
-        //formula taken from here.
-        // https://math.stackexchange.com/questions/432902/how-to-get-the-radius-of-an-ellipse-at-a-specific-angle-by-knowing-its-semi-majo
-        //trigonometry functions do not work with degrees
-        double a = ellipseWithConnectors.getRadiusY();
-        double b = ellipseWithConnectors.getRadiusX();
-        LOGGER.info("a in rad: " + a);
-        LOGGER.info("b in rad: " + b);
-        return (a * b) / Math.sqrt(
-                Math.pow(a , 2) * Math.pow(Math.sin(angleInRadians), 2) + Math.pow(b, 2) * Math.pow(Math.cos(angleInRadians), 2));
     }
 
     protected void setEllipseProperties(Ellipse ellipse) {
