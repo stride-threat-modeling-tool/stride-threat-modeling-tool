ec35d0601557a75b164624650fa17174ba7b5251
schrema8 <schrema8@students.zhaw.ch>
2021-04-14 17:47:11 +0200

    -updated to junit5

--- src/test/java/ch/zhaw/threatmodeling/MainControllerTest.java
+++ src/test/java/ch/zhaw/threatmodeling/MainControllerTest.java
@@ -4,36 +4,38 @@ import ch.zhaw.threatmodeling.controller.MainController;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.util.logging.Logger;
+
 
 public class MainControllerTest extends ApplicationTest {
 
     private App app;
     private MainController mainController;
     private static final Logger LOGGER = Logger.getLogger("Main Controller Test");
+
     @Override
     public void start (Stage stage) throws Exception {
         app = new App();
         app.start(stage);
         mainController = app.getController();
     }
-    @Before
+    @BeforeEach
     public void setUp () throws Exception {
     }
 
     @Test
     public void testGraphEditorSet() {
-        Assert.assertNull(mainController.getGraphEditor());
+        Assertions.assertNull(mainController.getGraphEditor());
     }
 
-    @After
+    @AfterEach
     public void tearDown () throws Exception {
         FxToolkit.hideStage();
         release(new KeyCode[]{});
